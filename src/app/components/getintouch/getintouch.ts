2f4d5f6432855e083b3c20554b95f477f2186dd0
Mark <markehabtawfiq@gmail.com>
2025-07-31 20:06:09 +0300

    'modal.css

---
+++ src/app/components/getintouch/getintouch.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-getintouch',
+  imports: [],
+  templateUrl: './getintouch.html',
+  styleUrl: './getintouch.css'
+})
+export class Getintouch {
+
+}
